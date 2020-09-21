16602eb7a861e3c71c16a3ed66193891c8da07b0
Matthew Little <m.a.litle94@gmail.com>
2020-09-21 13:09:47 -0400

    added onchange and parallelshell for automation purposes, also moved js scripts to seperate file

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+/* old tooltip script
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+*/
+
+
+$(function() {
+
+//Carousel Buttons
+    $('#mycarousel').carousel({interval: 2000});
+
+    $('#carouselButton').on('click', function() {
+        if($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+
+    //Reservation Button
+    $('#reserveButton').on('click', function() {
+        $('#reservationModal').modal('toggle');
+    });
+
+    //Login Link
+    $('#loginLink').on('click', function() {
+        $('#loginModal').modal('toggle');
+    });
+
+});
